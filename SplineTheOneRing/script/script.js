ccaf8f4d45390f7515b027f51bf0ee3db3e6b2e9
JakobHald <jako8022@mcdmstudent.dk>
2024-02-11 14:43:56 +0100

    publish cuh

--- SplineTheOneRing/script/script.js
+++ SplineTheOneRing/script/script.js
@@ -1,20 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.textContainer', {
-    x: -1000,
-    duration: 1,
-})
-
-gsap.from('.ringContainer', {
-    x: 1000,
-    duration: 1,
-    delay: 1
-})
-
-
-gsap.to('.textContainer', {
+gsap.fromTo('.textContainer', {
     opacity: 0,
+}, {
+    opacity: 1,
+    duration: 2,
+});
+
+
+gsap.fromTo('.textContainer', {
+    opacity: 1,
+}, {
     
+    opacity: 0,
     scrollTrigger: {
         trigger: '.textContainer',
         start: 'top top',
@@ -25,14 +23,30 @@ gsap.to('.textContainer', {
 
 
 
+gsap.from('.ringContainer', {
+    x: 1000,
+    duration: 1,
+    delay: 1
+})
 
 gsap.to('.theOneRing', {
     opacity: 0,
+    
     
     scrollTrigger: {
         trigger: '.textContainer',
         start: 'top top',
         end: 'bottom 10%',
         scrub: true,
+    }
+});
+
+gsap.from('.textContainer2', {
+    opacity: 0,
+    duration: 2,
+    scrollTrigger: {
+        trigger: '.textContainer2',
+        start: 'top center',
+        end: 'bottom bottom',
     }
 });
